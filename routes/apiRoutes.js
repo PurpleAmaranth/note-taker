133c786da294179984a9285e0faf1a66690fd580
PurpleAmaranth <36141949+PurpleAmaranth@users.noreply.github.com>
2021-06-09 17:56:33 -0600

    working on server.js, added html routes.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,7 @@
+module.exports = (app) => {
+//GET
+
+//POST
+
+//DELETE
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+// server launchpad
+
+const express = require('express');
+
+//instantiate express
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
+
+
