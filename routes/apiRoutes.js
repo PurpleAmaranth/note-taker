a55d7b353cff2cd652662e3bb1ffde62b8a365c9
PurpleAmaranth <36141949+PurpleAmaranth@users.noreply.github.com>
2021-07-25 23:37:57 -0600

    Added new routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,30 @@
 module.exports = (app) => {
-//GET
+    const router = require('express').Router();
+    const store = require('../db/store');
 
-//POST
+    //GET
+    app.get('/notes', (req, res) => {
+        store
+            .getNotes()
+            .then((notes) => {
+                return res.json(notes);
+            })
+            .catch((err) => res.status(500).json(err));
+    });
 
-//DELETE
+    //POST
+    app.post('/notes', (req, res) => {
+        store
+            .addNote(req.body)
+            .then((note) => res.json(note))
+            .catch((err) => res.status(500).json(err))
+    });
+
+    //DELETE
+    app.delete('/notes/:id', (req, res) => {
+        store
+            .removeNote(req.params.id)
+            .then(() => res.json({ ok: true }))
+            .catch((err) => res.status(500).json(err));
+    });
 }
